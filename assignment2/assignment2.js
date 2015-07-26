e10250977f99ec3d1aa8eb2b4ea78a06d29a4acc
Pradeep <pradeep.pyro@gmail.com>
2015-07-26 12:26:50 +0800

    Fix stroke colors

--- assignment2/assignment2.js
+++ assignment2/assignment2.js
@@ -1,31 +1,31 @@
 "use strict";
 var gl;
-var vbuffer;
 var program;
 var drawing = false;
 var pts = []
+var cols = []
 var canvas;
-var initBufferSize = 2000;
+var initBufferSize = 10000;
 var start_indices = [];
 
-function getRelPos(evt) {
-    if (evt.offsetX !== undefined && evt.offsetY !== undefined) {
-        return vec2(evt.offsetX, evt.offsetY);
-    } else {
-        return vec2(evt.layerX, evt.layerY);
-    }
-}
+// GPU buffers
+var vBuffer;
+var cBuffer;
 
-function relPosToNDC(pos) {
-    var cx = (2*pos[0]/canvas.width)-1;
-    var cy = (2* (canvas.height-pos[1])/ canvas.height)-1;
-    return vec2(cx, cy);
+// input variables
+var inColor = "#ff0000";
+
+function updateSettings() {
+    inColor = document.getElementById("lineColor").value;
+    console.log(inColor);
 }
 
 function drawStrokes(evt) {
     if (drawing) {
         var new_pt = relPosToNDC(getRelPos(evt))
+        var new_col = hexToRGB(inColor);
         pts.push(new_pt);
+        cols.push(new_col);
         updateBuffer();
         render();
     }
@@ -53,19 +53,27 @@ function render() {
 }
 
 function initBuffer() {
-    vbuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, initBufferSize, gl.DYNAMIC_DRAW);
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-    
-    updateBuffer();
+ 
+    // Load an empty color buffer onto the GPU
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, initBufferSize, gl.DYNAMIC_DRAW);
+    var vColor = gl.getAttribLocation(program, 'vColor');
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 }
 
 function updateBuffer() {
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(pts));
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(cols));
 }
 
 window.onload = function init() {
@@ -77,6 +85,7 @@ window.onload = function init() {
     }
 
     // register event handlers
+    document.getElementById('settings').addEventListener('change', updateSettings);
     canvas.addEventListener('mousedown', function(event) {
         start_indices.push(pts.length);
         drawing = true;
@@ -96,4 +105,30 @@ window.onload = function init() {
     initBuffer();
     
     render();
+}
+
+function getRelPos(evt) {
+    if (evt.offsetX !== undefined && evt.offsetY !== undefined) {
+        return vec2(evt.offsetX, evt.offsetY);
+    } else {
+        return vec2(evt.layerX, evt.layerY);
+    }
+}
+
+function relPosToNDC(pos) {
+    var cx = (2*pos[0]/canvas.width)-1;
+    var cy = (2* (canvas.height-pos[1])/ canvas.height)-1;
+    return vec2(cx, cy);
+}
+
+// http://www.javascripter.net/faq/hextorgb.htm
+function cutHex(h) {
+    return (h.charAt(0)=="#") ? h.substring(1,7):h;
+}
+function hexToRGB(h) {
+    var g = parseInt((cutHex(h)).substring(2,4),16)/255;
+    var b = parseInt((cutHex(h)).substring(4,6),16)/255;
+    var r = parseInt((cutHex(h)).substring(0,2),16)/255;
+    console.log(r, g, b);
+    return vec3(r, g, b);
 }
